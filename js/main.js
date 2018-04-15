30ec170f84a1a70d379d84eecc29e1e4c19d3439
Elyes Manai <manai.elye2s@gmail.com>
2018-04-15 14:50:36 +0100

    anis zed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-<script src="webcam.js"></script>
+  var config = {
+    apiKey: "AIzaSyBbV2f2rTZA-zZ9ZqmqbgeeOsK_xHfusCY",
+    authDomain: "n7eb-telifoun.firebaseapp.com",
+    databaseURL: "https://n7eb-telifoun.firebaseio.com",
+    projectId: "n7eb-telifoun",
+    storageBucket: "n7eb-telifoun.appspot.com",
+    messagingSenderId: "832985210635"
+  };
+  firebase.initializeApp(config);
+  	var i=0;
+  	var j = 0;
+  	console.log(j);
 
-	<div id="my_camera" style="width:320px; height:240px;"></div>
-	<div id="my_result"></div>
+var database = firebase.database();
+var ref = database.ref().child("personne");
+ref.on('value',function(snapshot){
+	console.log('snapshot : '+ JSON.stringify(snapshot.key));
+	//console.log('snapshot : '+ JSON.stringify(snapshot.key));
+	snapshot.forEach(function(item){
+		var type = JSON.stringify(item.val().type);
+		console.log(type);		
+		var sc = JSON.stringify(item.val().scoreclient);
+		console.log(sc);
+		var name = JSON.stringify(item.val().name);
+		document.getElementById("type").innerHTML=type.split('\"').join('');
+		document.getElementById("name").innerHTML=name.split('\"').join('');
+		document.getElementById("score").innerHTML=sc.split('\"').join('')+"%";
+	    var scoreGlobal = JSON.stringify(item.val().scoreGlobal);
+		var datetrans = JSON.stringify(item.val().transaction.dateT);
+		var feedback = JSON.stringify(item.val().transaction.feedback);
+		var score = JSON.stringify(item.val().transaction.score);
+		var nameprod = JSON.stringify(item.val().transaction.product.name); 
+		var state = JSON.stringify(item.val().transaction.product.state); 
+		var productowner = JSON.stringify(item.val().transaction.product.productowner); 
+		console.log(feedback);
+		var node = document.createElement("li");                 // Create a <li> node
+		var textnode = document.createTextNode(nameprod);         // Create a text node
+		node.appendChild(textnode);                              // Append the text to <li>
+		document.getElementById('produits').appendChild(node); 
+	});
 
-	<script language="JavaScript">
-		Webcam.attach( '#my_camera' );
-		
-		function take_snapshot() {
-			Webcam.snap( function(data_uri) {
-				document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
-			} );
-		}
-	</script>
+});
 
-	<a href="javascript:void(take_snapshot())">Take Snapshot</a>
+     
